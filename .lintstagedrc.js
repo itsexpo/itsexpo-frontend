32042da2ad6a8e06975af9defc6d606881da73cb
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-02-01 18:01:00 +0700

    ci: change typecheck to pre-push

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn typecheck',
-
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js)': () => [`yarn lint:strict`, `yarn format:write`],
 
