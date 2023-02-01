dac4e152c55beb1c3794f4793510e3ca42dcef33
Mikask <darrenprasetya40@gmail.com>
2023-02-01 19:36:49 +0700

    feat: styled IconButton to Figma

--- src/pages/sandbox/button.page.tsx
+++ src/pages/sandbox/button.page.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { FaChrome } from 'react-icons/fa';
 
 import Button from '@/components/buttons/Button';
+import IconButton from '@/components/buttons/IconButton';
+import Typography from '@/components/typography/Typography';
 import Layout from '@/layouts/Layout';
 
 export default function ButtonSandbox() {
   return (
     <Layout>
-      <section className=''>
-        <div className='layout min-h-screen py-20 flex flex-wrap justify-between'>
+      <section className='min-h-screen'>
+        <Typography variant='h4' className='layout text-white pt-10'>
+          Button
+        </Typography>
+        <div className='layout py-10 flex flex-wrap justify-between'>
           <div className='space-y-3 h-fit'>
             <div className='space-x-3'>
               <Button variant='red' size='large'>
@@ -112,6 +117,33 @@ export default function ButtonSandbox() {
               <Button variant='green' size='small' rightIcon={FaChrome}>
                 Text Only
               </Button>
+            </div>
+          </div>
+        </div>
+
+        <Typography variant='h4' className='layout text-white pt-10'>
+          IconButton
+        </Typography>
+        <div className='layout min-h-screen py-10 flex flex-wrap justify-between'>
+          <div className='space-y-3 h-fit'>
+            <div className='space-x-3'>
+              <IconButton variant='red' size='large' icon={FaChrome} />
+              <IconButton variant='red' size='base' icon={FaChrome} />
+              <IconButton variant='red' size='small' icon={FaChrome} />
+            </div>
+            <div className='space-x-3'>
+              <IconButton variant='yellow' size='large' icon={FaChrome} />
+              <IconButton variant='yellow' size='base' icon={FaChrome} />
+              <IconButton variant='yellow' size='small' icon={FaChrome} />
+            </div>
+            <div className='space-x-3'>
+              <IconButton variant='green' size='large' icon={FaChrome} />
+              <IconButton variant='green' size='base' icon={FaChrome} />
+              <IconButton
+                variant='green'
+                size='small'
+                icon={FaChrome}
+              ></IconButton>
             </div>
           </div>
         </div>
