26542799bb6f7408f73e4c8f6be7af64df66cbfc
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-02-20 16:13:15 +0700

    fix: optimize next font

---
+++ src/lib/font.ts
@@ -0,0 +1,11 @@
+import localFont from '@next/font/local';
+
+export const upakarti = localFont({
+  src: '../styles/fonts/upakarti.ttf',
+  variable: '--font-upakarti',
+});
+
+export const montserrat = localFont({
+  src: '../styles/fonts/montserrat.ttf',
+  variable: '--font-montserrat',
+});
