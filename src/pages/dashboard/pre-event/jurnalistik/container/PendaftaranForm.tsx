f58c6f9d94fa425123534670bdf62fed1de1f5c5
nathanaelpganata <nathanaelpg8@gmail.com>
2023-03-06 14:12:44 +0700

    feat: integrasi pendaftaran preevent jurnalistik

--- src/pages/dashboard/pre-event/jurnalistik/container/PendaftaranForm.tsx
+++ src/pages/dashboard/pre-event/jurnalistik/container/PendaftaranForm.tsx
@@ -1,4 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { serialize } from 'object-to-formdata';
 import * as React from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { FaUser, FaUsers } from 'react-icons/fa';
@@ -7,8 +9,10 @@ import Button from '@/components/buttons/Button';
 import DropzoneInput from '@/components/forms/DropzoneInput';
 import Input from '@/components/forms/Input';
 import SelectInput from '@/components/forms/SelectInput';
+import useMutationToast from '@/hooks/toast/useMutationToast';
 import api from '@/lib/api';
 import TeamRoleRadio from '@/pages/dashboard/pre-event/jurnalistik/components/TeamRoleRadio';
+import useAuthStore from '@/store/useAuthStore';
 import { ApiReturn } from '@/types/api';
 import { Jurnalistik } from '@/types/entities/pre-event/jurnalistik';
 
@@ -18,13 +22,79 @@ export default function FromPendaftaran() {
   >([]);
   const methods = useForm<Jurnalistik>();
 
-  const { handleSubmit, watch } = methods;
+  const {
+    handleSubmit,
+    watch,
+    formState: { isDirty },
+  } = methods;
+
+  const router = useRouter();
+
+  const user = useAuthStore.useUser();
+
+  const { mutate: SubmitAsKetua, isLoading: KetuaIsLoading } = useMutationToast<
+    void,
+    FormData
+  >(
+    useMutation((data) => {
+      return api.post('pre_event/jurnalistik/ketua', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+    })
+  );
+
+  const { mutate: SubmitAsMember, isLoading: MemberIsLoading } =
+    useMutationToast<void, FormData>(
+      useMutation((data) => {
+        return api.post('pre_event/jurnalistik/member', data, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+      })
+    );
 
   const onSubmit = (data: Jurnalistik) => {
-    // eslint-disable-next-line no-console
-    console.log(data);
+    const image = {
+      id_card: data.id_card[0],
+      follow_sosmed: data.follow_sosmed[0],
+      share_poster: data.share_poster[0],
+    };
+    const lomba_category = user?.role_id == '5' ? 'TELEVISION' : 'BLOGGER';
+
+    if (isTeamLead) {
+      const body = {
+        name: data.name,
+        member_type: data.member_type,
+        team_name: data.team_name,
+        lomba_category: lomba_category,
+        jenis_kegiatan: data.jenis_kegiatan,
+        provinsi_id: data.provinsi_id,
+        kabupaten_id: data.kabupaten_id,
+        asal_instansi: data.asal_instansi,
+        id_line: data.id_line,
+        ...image,
+      };
+      const formData = serialize(body);
+      SubmitAsKetua(formData);
+    } else {
+      const body = {
+        name: data.name,
+        member_type: data.member_type,
+        provinsi_id: data.provinsi_id,
+        kabupaten_id: data.kabupaten_id,
+        asal_instansi: data.asal_instansi,
+        id_line: data.id_line,
+        ...image,
+      };
+      const formData = serialize(body, { indices: true });
+      SubmitAsMember(formData);
+    }
+    router.push('/dashboard/pre-event/jurnalistik');
   };
-  const isTeamLead = watch('team_role') === 'ketua';
+  const isTeamLead = watch('member_type') === 'ketua';
 
   const provinsi = useQuery<ApiReturn<{ id: string; name: string }[]>>([
     '/provinsi',
@@ -41,12 +111,12 @@ export default function FromPendaftaran() {
   };
 
   return (
-    <div className='grid grid-cols-5'>
+    <div className='grid grid-cols-1 md:grid-cols-5'>
       <div className='col-span-3 bg-white shadow-pendaftaran p-6 rounded-xl'>
         <FormProvider {...methods}>
           <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
             <TeamRoleRadio
-              id='team_role'
+              id='member_type'
               label='Daftar Sebagai'
               options={[
                 { label: 'Ketua', value: 'ketua', icon: FaUser },
@@ -71,14 +141,14 @@ export default function FromPendaftaran() {
             <SelectInput
               id='jenis_kegiatan'
               label='Jenis Kegiatan'
-              validation={{ required: 'Nama tidak boleh kosong' }}
+              validation={{ required: 'Jenis Kegiatan tidak boleh kosong' }}
             >
               <option value=''>Pilih Jenis Kegiatan</option>
               <option value='KHUSUS'>Kegiatan Khusus</option>
-              <option value='UMUM'>Kegiatan UMUM</option>
+              <option value='UMUM'>Kegiatan Umum</option>
             </SelectInput>
             <SelectInput
-              id='provinsi'
+              id='provinsi_id'
               label='Provinsi'
               validation={{ required: 'Provinsi tidak boleh kosong' }}
               onChange={(e) => getKabupaten(e.target.value)}
@@ -91,7 +161,7 @@ export default function FromPendaftaran() {
             </SelectInput>
             {kabupaten.length > 0 && (
               <SelectInput
-                id='kabupaten'
+                id='kabupaten_id'
                 label='Kabupaten'
                 validation={{ required: 'Kabupaten tidak boleh kosong' }}
                 onChange={(e) => getKabupaten(e.target.value)}
@@ -136,7 +206,13 @@ export default function FromPendaftaran() {
               }}
             />
 
-            <Button type='submit'>Submit</Button>
+            <Button
+              type='submit'
+              disabled={!isDirty}
+              isLoading={KetuaIsLoading || MemberIsLoading}
+            >
+              Submit
+            </Button>
           </form>
         </FormProvider>
       </div>
