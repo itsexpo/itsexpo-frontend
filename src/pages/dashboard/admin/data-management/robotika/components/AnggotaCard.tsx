41aab5197da5ed1c9935967a97bfa4bcb65ba1da
Mikask <darrenprasetya40@gmail.com>
2023-05-03 14:11:35 +0700

    feat: add phone number to anggota card

--- src/pages/dashboard/admin/data-management/robotika/components/AnggotaCard.tsx
+++ src/pages/dashboard/admin/data-management/robotika/components/AnggotaCard.tsx
@@ -3,17 +3,22 @@ import Typography from '@/components/typography/Typography';
 export default function AnggotaCard({
   jabatan,
   nama,
+  phone_number,
 }: {
   jabatan: string;
   nama: string;
+  phone_number: string;
 }) {
   return (
     <div className='p-4 rounded-lg border border-typo-outline'>
-      <Typography as='c' variant='c' className='font-medium text-typo-icon'>
+      <Typography as='p' variant='c' className='font-medium text-typo-icon'>
         {jabatan}
       </Typography>
       <Typography className='font-semibold text-typo-primary'>
         {nama}
+      </Typography>
+      <Typography as='p' className='text-typo-secondary text-[11px]'>
+        {phone_number}
       </Typography>
     </div>
   );
